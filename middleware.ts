8b7a23f547dea48bef99765d4e04f90202c9ebe7
ashbur <18829526908@163.com>
2024-05-22 09:16:25 +0800

    移除已经失效的代码写法

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,12 @@
-import { authMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware,createRouteMatcher } from "@clerk/nextjs/server";
 
-export default authMiddleware({
-  publicRoutes: ["/api/uploadthing"]
+const isPublicRoute = createRouteMatcher(['/sign-in', '/sign-up','/api/uploadthing']);
+
+
+export default clerkMiddleware((auth, request) => {
+  if(!isPublicRoute(request)) {
+    auth().protect();
+  }
 });
 
 export const config = {
--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: [
-            "uploadthing.com","utfs.io"
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: "uploadthing.com",
+                port: '',
+                pathname: '/**'
+            },
+            {
+                protocol: 'https',
+                hostname: "utfs.io",
+                port: '',
+                pathname: '/**'
+            }
         ]
     }
 };
