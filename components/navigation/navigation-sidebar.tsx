01f03cd8411b0cea2561cde6e36b51d617bf43fe
ashbur <18829526908@163.com>
2024-05-26 20:14:53 +0800

    一些样式 bug 修复

--- components/navigation/navigation-sidebar.tsx
+++ components/navigation/navigation-sidebar.tsx
@@ -25,7 +25,7 @@ export const NavigationSidebar = async () => {
         }
     })
     return (
-        <div className="space-y-4 flex flex-col items-center h-full text-primary w-full dark:bg-[#1E1F22] py-3">
+        <div className="space-y-4 flex flex-col items-center h-full text-primary w-full dark:bg-[#1E1F22] bg-[#E3E5E8] py-3">
             <NavigationAction />
             <Separator 
                 className="h-[2px] bg-zinc-300 dark:bg-zinc-700 rounded-md w-10 mx-auto"
