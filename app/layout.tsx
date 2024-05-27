53bb98b9179267fd99c9acdac1baba8deb1ba2b4
ashbur <18829526908@163.com>
2024-05-27 09:27:41 +0800

    完成 socket io的引入与配置

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { ClerkProvider } from "@clerk/nextjs";
 import { ThemeProvider } from "@/components/providers/theme-provider";
 import { cn } from "@/lib/utils";
 import { ModalProvider } from "@/components/providers/modal-provider";
+import { SocketProvider } from "@/components/providers/socket-provider";
 const font = Open_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -27,8 +28,10 @@ export default function RootLayout({
               enableSystem={false}
               storageKey="discord-theme"
               >
-              <ModalProvider />
-                {children}
+              <SocketProvider>
+                <ModalProvider />
+                  {children}
+              </SocketProvider>
             </ThemeProvider>
           </body>
         </html>
--- components/chat/chat-header.tsx
+++ components/chat/chat-header.tsx
@@ -1,6 +1,7 @@
 import { Hash, Menu } from "lucide-react";
-import MobileToggle from "../mobile-toggle";
-import { UserAvatar } from "../user-avatar";
+import MobileToggle from "@/components/mobile-toggle";
+import { UserAvatar } from "@/components/user-avatar";
+import SocketIndicator from "@/components/socket-indicator";
 
 interface ChatHeaderProps {
     serverId: string;
@@ -27,6 +28,9 @@ export const ChatHeader = ({ serverId, name, type, imageUrl }: ChatHeaderProps) 
             <p className="font-semibold text-md text-black dark:text-white">
                 {name}
             </p>
+            <div className=" ml-auto flex items-center">
+                <SocketIndicator />
+            </div>
         </div>
     )
 }
