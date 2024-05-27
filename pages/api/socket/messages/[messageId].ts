99548cccb751e514a51801e9a788aecf9abeb791
ashbur <18829526908@163.com>
2024-05-27 20:31:17 +0800

    完成消息处理函数

--- pages/api/socket/messages/[messageId].ts
+++ pages/api/socket/messages/[messageId].ts
@@ -140,8 +140,6 @@ export default async function handler(
         res?.socket?.server?.io?.emit(updateKey, message);
 
         return res.status(200).json(message);
-
-
     }catch (error) {
         console.log("[MESSAGE_ID]", error);
         return res.status(500).json({ error: "Internal Error" });
